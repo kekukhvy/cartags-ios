3436a24a6c45d563f76160d906c374da3771fd85
vladyslavkekukh <kekukhvy@gmail.com>
2026-03-28 21:44:20 +0100

    Initial Commit

---
+++ CarTags/CarTagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTagsApp.swift
+//  CarTags
+//
+//  Created by Vladyslav Kekukh on 28.03.26.
+//
+
+import SwiftUI
+
+@main
+struct CarTagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
